d37e095c1897e2b0702593e64032186391902d1c
spitice <spitice@outlook.jp>
2025-05-23 21:04:04 +0900

    feat: External View v2
    - fix: Correct the location from which attack/use hittrace starts while using external view (req. ExternalViewHelper)
    - fix: Issue with special items that gets removed after using observer view
    - fix: Reduce camera jitter when using !g on player who is moving fast
    - feat: Add !mv command
    - feat: Add !fc (!freecam) command
    - feat: Add !camdist command
    - feat: Add !tpq command and improve !tp/!tpp command behavior
    - feat: Improve !g (!watch) command behavior
    - feat: Mouse click to cycle players in !g camera
    - feat: Add several ConVars for tuning ExternalView plugin
    - refactor: Move some code from shared MathUtils to ExternalView MathUtils

---
+++ LupercaliaMGCore/modules/ExternalView/API/IExternalViewConVars.cs
@@ -0,0 +1,15 @@
+﻿namespace LupercaliaMGCore.modules.ExternalView.API
+{
+    public interface IExternalViewConVars
+    {
+        float ThirdPersonMinDistance { get; }
+        float ThirdPersonMaxDistance { get; }
+        float ModelViewCameraSpeed { get; }
+        float ModelViewCameraAltSpeed { get; }
+        float ModelViewCameraRadius { get; }
+        float FreeCameraSpeed { get; }
+        float FreeCameraAltSpeed { get; }
+        bool IsObserverViewEnabled { get; }
+        bool IsAdminPrevilegesEnabled { get; }
+    }
+}
---
+++ LupercaliaMGCore/modules/ExternalView/Cameras/ICameraContext.cs
@@ -0,0 +1,13 @@
+﻿using LupercaliaMGCore.modules.ExternalView.API;
+using LupercaliaMGCore.modules.ExternalView.Player;
+
+namespace LupercaliaMGCore.modules.ExternalView.Cameras
+{
+    public interface ICameraContext
+    {
+        public IExternalViewCsApi Api { get; }
+        public IExternalViewCsEntity? CameraEntity { get; }
+        public IExternalViewCsPlayer Player { get; }
+        public PlayerConfig Config { get; }
+    }
+}
